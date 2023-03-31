dba0e4a3d2a7c07422cd192a5204dae62e274f7b
Adnan Alouane <alouane@usc.edu>
2023-03-31 05:52:06 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,4 +1,5 @@
-#ifndef RECCHECK
+
+
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -6,23 +7,75 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#endif
-
-// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
+// Prototypes for helper functions
+bool scheduleHelper( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t currentDay, size_t currentWorkers );
 
+bool isValid(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t currentDay, size_t currentWorkers, Worker_T id );
 
 // Add your implementation of schedule() and other helper functions here
+
+bool isValid( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t currentDay, size_t currentWorkers, Worker_T id )
+{
+	size_t count = 0;
+	for (size_t i = 0; i < sched.size(); ++i)
+	{
+		for (size_t j = 0; j < dailyNeed; ++j)
+		{
+			if (sched[i][j] == id)
+			{
+				count++;
+			}
+		}
+	}
+	return (count <= maxShifts);
+}
+
+bool scheduleHelper( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t currentDay, size_t currentWorkers )
+{
+	if (currentDay == sched.size())
+	{
+		return true;
+	}
+		for (size_t i = 0; i < avail[0].size(); ++i)
+		{
+			if (avail[currentDay][i]){
+				if (find(sched[currentDay].begin(), sched[currentDay].end(), i) != sched[currentDay].end())
+				{
+					continue;
+				}
+				sched[currentDay][currentWorkers] = i;
+				if (isValid(avail, dailyNeed, maxShifts, sched, currentDay, currentWorkers, i))
+				{
+					if (currentWorkers == dailyNeed - 1)
+					{
+						if (scheduleHelper(avail, dailyNeed, maxShifts, sched, currentDay + 1, 0))
+						{
+							return true;
+						}
+					}
+					else
+					{
+						if (scheduleHelper(avail, dailyNeed, maxShifts, sched, currentDay, currentWorkers + 1))
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+		sched[currentDay][currentWorkers] = INVALID_ID;
+		return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -35,10 +88,10 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+		std::vector<Worker_T> init(dailyNeed, INVALID_ID);
+    for (size_t row = 0; row < avail.size(); ++row)
+    {
+				sched.push_back(init);
+    }
+		return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -1,19 +1,18 @@
-#ifndef RECCHECK
 // For debugging
 #include <iostream>
 // For std::remove
 #include <algorithm> 
 #include <map>
 #include <set>
-#endif
-
 #include "wordle.h"
 #include "dict-eng.h"
+//#include <cstring>
 using namespace std;
 
 
 // Add prototypes of helper functions here
 
+void charCombiner(std::string in, unsigned int currentIndex, std::string floatingCopy, const std::set<std::string>& dict, std::set<std::string>& answers);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +20,71 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+	std::set<std::string> validWords;
 
+	std::string input = in;
+	std::string floatingChars = floating;
+	charCombiner(input, 0, floatingChars, dict, validWords);
+	return validWords;
 }
 
 // Define any helper functions here
+
+void charCombiner(std::string in, unsigned int currentIndex, std::string floatingCopy, const std::set<std::string>& dict, std::set<std::string>& answers)
+{
+	unsigned int count = 0;
+	char character;
+	for (unsigned int i = 0; i < in.size(); ++i)
+	{
+		character = in[i];
+		if (character == '-')
+		{
+			count++;
+		}
+	} 
+	if (currentIndex == in.size())
+	{
+		std::set<std::string>::iterator dictFind = dict.find(in);
+		if (dictFind != dict.end())
+		{
+			answers.insert(in);
+		}
+		return;
+	}
+	character = in[currentIndex];
+	if (character != '-')
+	{
+			charCombiner(in, currentIndex + 1, floatingCopy, dict, answers);
+			return;
+	} else
+	{
+      if (count == floatingCopy.size())
+      {
+        for (unsigned int j = 0; j < floatingCopy.size(); ++j)
+        {
+          in[currentIndex] = floatingCopy[j];
+          std::string newFloatingCopy = floatingCopy;
+          newFloatingCopy.erase(j, 1);
+          charCombiner(in, currentIndex + 1, newFloatingCopy, dict, answers); 
+        }
+		}	else
+		{
+			for (int i = 0; i < 26; ++i)
+			{
+				char c = char(i + 97);
+				in[currentIndex] = c;
+				unsigned int floatingIndex = floatingCopy.find(c);
+				if (floatingIndex <= floatingCopy.size())
+				{
+					std::string newFloatingCopy = floatingCopy;
+					newFloatingCopy.erase(floatingIndex, 1);
+					charCombiner(in, currentIndex + 1, newFloatingCopy, dict, answers); 
+				}
+				else
+				{
+					charCombiner(in, currentIndex + 1, floatingCopy, dict, answers); 
+				}
+			}
+		}
+	}
+}
